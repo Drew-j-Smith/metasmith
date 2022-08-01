c50c3342069e6a8536e7c7ff2d61bb1bc6a1c8d2
Drew Smith <smithdrew555@yahoo.com>
2022-07-31 22:33:39 -0400

    simplifed code

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,63 @@
 
-#include "base.h"
-
+#include <array>
 #include <iostream>
+#include <string_view>
+#include <type_traits>
 
 using namespace std::literals;
 
-struct S : metasmith::Base<S> {
+template <typename T, typename PtrType, PtrType T::*ptr> struct FieldRef {};
+
+class Field {
+private:
+    std::string_view key;
+    const void *(*get_fp)(const void *);
+    void (*set_fp)(void *, const void *);
+
+    template <typename T, typename PtrType, PtrType T::*ptr>
+    constexpr static const void *get_field(const void *ref) {
+        return &(static_cast<const T *>(ref)->*ptr);
+    }
+
+    template <typename T, typename PtrType, PtrType T::*ptr>
+    constexpr static void set_field(void *ref, const void *val) {
+        static_cast<T *>(ref)->*ptr = *static_cast<const PtrType *>(val);
+    }
+
+public:
+    template <typename T, typename PtrType, PtrType T::*ptr>
+    constexpr Field(std::string_view key, FieldRef<T, PtrType, ptr>)
+        : key(key), get_fp(get_field<T, PtrType, ptr>),
+          set_fp(set_field<T, PtrType, ptr>) {}
+
+    template <typename PtrType, typename T>
+    constexpr PtrType get(const T &ref) const {
+        return *static_cast<const PtrType *>(get_fp(&ref));
+    }
+
+    template <typename T, typename PtrType>
+    constexpr void set(T &ref, const PtrType &val) {
+        set_fp(&ref, &val);
+    }
+
+    constexpr auto get_key() { return key; }
+};
+
+struct S {
     int m_int;
     float m_float;
 
-    static constexpr auto fields =
-        gen_fields("int", &S::m_int, "float", &S::m_float);
+    constexpr static auto fields =
+        std::array{Field{"int"sv, FieldRef<S, int, &S::m_int>{}},
+                   Field{"float"sv, FieldRef<S, float, &S::m_float>{}}};
 };
 
 int main() {
-
     S s;
-    s.m_int = 1;
 
-    for (metasmith::Field field : S::get_fields()) {
+    for (auto field : S::fields) {
         if (field.get_key() == "int"sv) {
+            field.set(s, 1);
             std::cout << field.get<int>(s) << '\n';
         }
     }
