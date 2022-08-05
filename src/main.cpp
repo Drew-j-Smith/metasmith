296e0d4ba2d43588b6e415ed3189e276e49d8276
Drew Smith <smithdrew555@yahoo.com>
2022-08-04 20:10:04 -0400

    added universal print

--- src/main.cpp
+++ src/main.cpp
@@ -6,53 +6,74 @@
 
 using namespace std::literals;
 
-template <auto ptr> struct FieldRef {};
+template <auto ptr> struct FieldRef { std::string_view key; };
 
-class Field {
+template <typename T> class Field {
 private:
     std::string_view key;
-    const void *(*get_fp)(const void *);
-    void (*set_fp)(void *, const void *);
+    const void *(*get_fp)(const T &);
+    void (*set_fp)(T &, const void *);
+    void (*print_fp)(T &, std::ostream &);
 
-    template <typename T, typename PtrType, PtrType T::*ptr>
-    static const void *get_field(const void *ref) {
-        return &(static_cast<const T *>(ref)->*ptr);
+    template <auto ptr> static const void *get_field(const T &ref) {
+        return &(ref.*ptr);
     }
 
-    template <typename T, typename PtrType, PtrType T::*ptr>
-    static void set_field(void *ref, const void *val) {
-        static_cast<T *>(ref)->*ptr = *static_cast<const PtrType *>(val);
+    template <typename PtrType, PtrType T::*ptr>
+    static void set_field(T &ref, const void *val) {
+        ref.*ptr = *static_cast<const PtrType *>(val);
+    }
+
+    template <auto ptr> static void print_field(T &ref, std::ostream &out) {
+        out << ref.*ptr;
     }
 
 public:
-    template <typename T, typename PtrType, PtrType T::*ptr>
-    constexpr Field(std::string_view key, FieldRef<ptr>)
-        : key(key), get_fp(get_field<T, PtrType, ptr>),
-          set_fp(set_field<T, PtrType, ptr>) {}
+    template <typename PtrType, PtrType T::*ptr>
+    constexpr Field(FieldRef<ptr> field)
+        : key(field.key), get_fp(get_field<ptr>),
+          set_fp(set_field<PtrType, ptr>), print_fp(print_field<ptr>) {}
 
-    template <typename PtrType, typename T> PtrType get(const T &ref) const {
-        return *static_cast<const PtrType *>(get_fp(&ref));
+    template <typename PtrType> PtrType get(const T &ref) const {
+        return *static_cast<const PtrType *>(get_fp(ref));
     }
 
-    template <typename T, typename PtrType>
-    void set(T &ref, const PtrType &val) {
-        set_fp(&ref, &val);
+    template <typename PtrType> void set(T &ref, const PtrType &val) {
+        set_fp(ref, &val);
     }
+
+    void print(T &ref, std::ostream &out) { print_fp(ref, out); }
 
     constexpr auto get_key() { return key; }
 };
+
+template <typename T>
+concept Base = requires(T t) {
+    { t.name } -> std::convertible_to<std::string_view>;
+    []<std::size_t size>(std::array<Field<T>, size>) {}(t.fields);
+};
+
+template <Base T> std::ostream &operator<<(std::ostream &out, T t) {
+    out << t.name << " { ";
+    for (auto field : T::fields) {
+        out << field.get_key() << ": ";
+        field.print(t, out);
+        out << ", ";
+    }
+    return out << "}";
+}
 
 struct S {
     int m_int;
     float m_float;
 
-    constexpr static auto fields =
-        std::array{Field{"int"sv, FieldRef<&S::m_int>{}},
-                   Field{"float"sv, FieldRef<&S::m_float>{}}};
+    constexpr static auto name = "S";
+    constexpr static std::array<Field<S>, 2> fields{
+        FieldRef<&S::m_int>{"int"}, FieldRef<&S::m_float>{"float"}};
 };
 
 int main() {
-    S s;
+    S s{0, 1.1f};
 
     for (auto field : S::fields) {
         if (field.get_key() == "int"sv) {
@@ -60,4 +81,5 @@ int main() {
             std::cout << field.get<int>(s) << '\n';
         }
     }
+    std::cout << s;
 }
